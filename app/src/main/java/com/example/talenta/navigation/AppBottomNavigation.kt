13808f89d7cbd5e6ddd8d8cbbde6fb09fca1b908
Narendra <narendramadugula2004@gmail.com>
2025-06-02 21:25:30 +0530

    Expert Details tab : fetching data from the data base

--- app/src/main/java/com/example/talenta/navigation/AppBottomNavigation.kt
+++ app/src/main/java/com/example/talenta/navigation/AppBottomNavigation.kt
@@ -2,19 +2,10 @@ package com.example.talenta.navigation
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.ui.*
 import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -24,14 +15,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -43,15 +32,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -65,7 +49,6 @@ import com.example.talenta.navigation.Routes.BottomNavRouteSponsor
 import com.example.talenta.navigation.Routes.Route
 import com.example.talenta.presentation.ui.screens.HostViewModel
 import com.example.talenta.presentation.ui.screens.isEqualTo
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun BottomNavBar(modifier: Modifier = Modifier, viewModel: HostViewModel = hiltViewModel(), navController: NavController) {
@@ -153,7 +136,7 @@ fun AppBottomNavigationForArtist(navController: NavController) {
                 brush = Brush.horizontalGradient(
                     colors = listOf(
                         Color(0xFF667EEA),
-                        Color(0xFF764BA2)
+                        Color(0xFF667EEA)
 
                     )
                 ),
